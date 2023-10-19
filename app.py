3de1c3214e2ef1f143f05d02fc58a97c3543941a
Muhammad Fakhrul Amin <80448871+mfakhrulam@users.noreply.github.com>
2023-10-19 18:48:46 +0700

    add vercel.json and app.py

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+from utils import get_stopwords_removal, get_lemmatized_stemmed_word
+import nltk
+from nltk.tokenize import word_tokenize
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+@app.route("/index", methods=["GET", "POST"])
+def index():
+  if request.method == "GET":
+    return render_template("index.html", sentence="")
+  
+  else:
+    lang = request.form['language']
+    stem_lemm_type=''
+    if lang == "english":
+      stem_lemm_type = request.form['stemmer_lemmatizer_en']
+    if lang == "indonesia":
+      stem_lemm_type = request.form['stemmer_lemmatizer_id']
+        
+    sentence = request.form['sentence']
+    case_folding = sentence.lower()
+    tokenize_word = [w for w in word_tokenize(case_folding) if w.isalnum()]
+    stopword_removal_word = get_stopwords_removal(tokenize_word, lang)
+    stem_lemm_word = get_lemmatized_stemmed_word(stopword_removal_word, stem_lemm_type)
+    
+  return render_template("index.html", 
+                         sentence=sentence, 
+                         lang=lang,
+                         stem_lemm_type=stem_lemm_type,
+                         case_folding=case_folding, 
+                         tokenize_word=tokenize_word, 
+                         stopword_removal_word=stopword_removal_word, 
+                         stem_lemm_word=stem_lemm_word)
+  
+
+if __name__ == "__main__":
+  nltk.download('punkt')
+  nltk.download('stopwords')
+  nltk.download('wordnet')
+  app.run(debug = True)
